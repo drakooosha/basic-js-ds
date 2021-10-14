7b5003f799306a1b3e1839c2120c1deb542ce83c
Kirill Kopteloy <kopteloy@gmail.com>
2021-10-15 00:16:58 +0300

    feat: list

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+    while(l.value == k) {
+      l = l.next;
+    }
+  
+    let Node = l;
+    let nextNode = l.next;
+    while(nextNode!=null) {
+      if(nextNode.value == k) {
+        Node.next = nextNode.next;
+        if(Node.next == null) {
+          return l;
+        }
+      }
+      Node = Node.next;
+      nextNode = Node.next;
+    }
+    return l;
 }
